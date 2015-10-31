a6c1ab3c5ae857534c2c284358f2ef5303e311f3
girishramnani <girishramnani95@gmail.com>
2015-10-31 16:54:31 +0530

    schema created

---
+++ model/videos.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var videoSchema = new Schema({
+
+  name: String,
+  description: String,
+  likes: { type :Number , default:0 },
+  createdOn : { type: Date , default: Date.now },
+  category: String
+
+});
+
+
+var Videos = mongoose.model('Videos',videoSchema);
+
+module.exports = Videos;
